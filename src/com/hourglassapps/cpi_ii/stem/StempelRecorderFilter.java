aec5eeb46a4868635327752d7b4bad0f0bfa3818
Kieran J. White <kieran.white@hourglassapps.com>
2014-10-15 17:32:10 +0100

    remove local version of StempelFilter -- now we use Lucene's StempelFilter

--- src/com/hourglassapps/cpi_ii/stem/StempelRecorderFilter.java
+++ src/com/hourglassapps/cpi_ii/stem/StempelRecorderFilter.java
@@ -8,46 +8,12 @@ import java.util.Set;
 
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.stempel.StempelFilter;
 import org.apache.lucene.analysis.stempel.StempelStemmer;
 
-import com.hourglassapps.cpi_ii.stem.lucene.AbstractStempelFilter;
 import com.hourglassapps.util.MultiMap;
 
 public final class StempelRecorderFilter extends StemRecorderFilter {
-	private final class StempelFilter extends AbstractStempelFilter implements PreFilter {
-		private CharSequence mInputToken=null;
-		
-		public StempelFilter(TokenStream in, StempelStemmer stemmer) {
-			super(in, stemmer);
-		}
-
-
-		public StempelFilter(TokenStream in, StempelStemmer stemmer,
-				int minLength) {
-			super(in, stemmer, minLength);
-		}
-
-
-		/** Returns the next input Token, after being stemmed */
-		@Override
-		public boolean incrementToken() throws IOException {
-			if (input.incrementToken()) {
-				mInputToken=termAtt;
-				stem();
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		@Override
-		public CharSequence priorToken() {
-			assert mInputToken!=null;
-			return mInputToken;
-		}
-
-	}
-
 	public StempelRecorderFilter(TokenStream pInput) throws IOException {
 		super(pInput);
 	}
